25f2ce3db6190c6ad6c6db3461f2c8c34d38e3ff
akshaysoni001 <akshaysoni460@gmail.com>
2022-03-05 13:03:01 +0530

    TinDog Complete

---
+++ main.js
@@ -0,0 +1,30 @@
+// console.log("Hello World");
+// console.error("Error");
+// console.warn("Warning");
+
+// Variables
+var x;
+
+let y = 20;
+x = 20;
+y = "String";
+const z = 00;
+console.log(x, y, z);
+
+console.log(typeof x, typeof y, typeof z);
+
+name = "Akshay";
+age = 30;
+
+console.log(`my name is ${name} and i'm ${age} year's old`);
+
+const arr = new Array("Akshay", "Soni", 25);
+console.log(arr, typeof arr);
+
+const arr1 = ["Soni", "Akshay", 25];
+console.log(arr1, typeof arr1);
+
+console.log(arr[1], arr1[1]);
+
+arr[1] = "Akshay";
+console.log(arr);
